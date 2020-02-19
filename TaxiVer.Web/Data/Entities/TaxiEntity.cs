95c335db576df65ac648b63bb3617f8ee70f1655
Pablo Manuel Arango Lara <pablito1194@hotmail.com>
2020-02-19 14:24:27 -0600

    Improve taxis controller

---
+++ TaxiVer.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaxiVer.Web.Data.Entities
+{
+    public class TaxiEntity
+    {
+        public int Id { get; set; }
+
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "The {0} field must have {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [RegularExpression(@"^([A-Za-z]{3}\d{3})$", ErrorMessage = "The field {0} must starts with three characters and ends with three numbers.")]
+        public string Plaque { get; set; }
+    }
+}
